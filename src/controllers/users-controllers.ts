b17c039bd7b7875f969f4b3ead366e476b23353f
luizroquim <luizpheliperoquim@gmail.com>
2025-12-03 12:53:20 -0300

    Initial commit

---
+++ src/controllers/users-controllers.ts
@@ -0,0 +1,7 @@
+import {Request, Response} from "express"
+class UsersController{
+    create(request: Request, response: Response){
+        return response.json({message:"Ok"})
+    }
+}
+export {UsersController}
