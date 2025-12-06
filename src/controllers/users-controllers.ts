1b9882cdff10466e0234d1085c892e8c5972bf64
luizroquim <luizpheliperoquim@gmail.com>
2025-12-06 05:59:12 -0300

    chore: save project before configuring prisma driver adapter

--- src/controllers/users-controllers.ts
+++ src/controllers/users-controllers.ts
@@ -1,6 +1,8 @@
 import {Request, Response} from "express"
+import { prisma } from "@/database/prisma"
 import {z} from "zod"
 import {hash} from "bcrypt"
+import { AppError } from "@/utils/AppError"
 class UsersController{
    async create(request: Request, response: Response){
         
@@ -12,9 +14,22 @@ class UsersController{
 
         const {name, email, password} = bodySchema.parse(request.body)
 
+        const userWithSameEmail = await prisma.user.findFirst({where:{email}})
+        if(userWithSameEmail){
+            throw new AppError("User with same email already exists")
+        }
+
         const hashedPassword = await hash(password,8)
 
-        return response.json({message:"Ok",hashedPassword})
+        const user = await prisma.user.create({
+            data:{
+                name,
+                email,
+                password:hashedPassword
+            }
+        })
+
+        return response.json({user})
     }
 }
 export {UsersController}
