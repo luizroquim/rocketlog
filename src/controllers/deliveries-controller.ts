ae27001249307b296b1add549fc93bf1ae424ac4
luizroquim <luizpheliperoquim@gmail.com>
2025-12-10 06:28:23 -0300

    feat:add route to list deliveries

--- src/controllers/deliveries-controller.ts
+++ src/controllers/deliveries-controller.ts
@@ -1,7 +1,28 @@
-import {Request,Response} from "express"
-class DeliveriesController{
-    create(request:Request, response:Response){
-        return response.json({message:"ok"})
-    }
+import { Request, Response } from "express";
+import { prisma } from "@/database/prisma";
+import { z } from "zod";
+class DeliveriesController {
+  async create(request: Request, response: Response) {
+    const bodySchema = z.object({
+      user_id: z.string().uuid(), //id do usuário que será enviado e não logado
+      description: z.string(),
+    });
+
+    const { user_id, description } = bodySchema.parse(request.body);
+
+    await prisma.delivery.create({
+      data: {
+        userId: user_id,
+        description,
+      },
+    });
+    return response.status(201).json();
+  }
+
+  async index(request:Request,response:Response){
+    const deliveries = await prisma.delivery.findMany()
+
+    return response.json(deliveries)
+  }
 }
-export{DeliveriesController}
+export { DeliveriesController };
