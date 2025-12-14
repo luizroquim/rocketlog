8b75ff6d964b2e43af77738bc1ec0dc353dddc9b
luizroquim <luizpheliperoquim@gmail.com>
2025-12-14 08:35:04 -0300

    chore: start deployment setup

--- src/env.ts
+++ src/env.ts
@@ -1,9 +1,10 @@
 import 'dotenv/config'
-import { z } from "zod";
+import { coerce, z } from "zod";
 
 const envSchema = z.object({
     DATABASE_URL: z.string().url(),
     JWT_SECRET:z.string(),
+    PORT:z.coerce.number().default(3333)
 })
 
 export const env = envSchema.parse(process.env)
